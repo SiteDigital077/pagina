b757f22906923bbf2a8bf44f3bb6613c53374dd5
SiteDigital077 <62308263+SiteDigital077@users.noreply.github.com>
2021-08-05 12:59:58 -0500

    Ajustes pagina
    Ajustes pagina

--- src/Http/WebController.php
+++ src/Http/WebController.php
@@ -924,8 +924,8 @@ $categories = \DigitalsiteSaaS\Pagina\Tenant\Pais::all();
    $contenido = Avanzaempresa::where('slug','=',$page)->get();
    $menu = Page::whereNull('page_id')->orderBy('posta', 'asc')->get();
    $blogfoot = Bloguero::inRandomOrder()->take(6)->get();
-   $usuario = Auth::user()->id;
-   $identificador = Avanzaempresa::where('usuario_id', '=', $usuario)->get();
+ 
+   $identificador = Avanzaempresa::where('slug', '=', $page)->get();
    foreach ($identificador as $identificador){
    $productos = Fichaje::where('identificador','=',$identificador->id)->get();
    }
@@ -938,8 +938,8 @@ $categories = \DigitalsiteSaaS\Pagina\Tenant\Pais::all();
    $contenido = \DigitalsiteSaaS\Avanza\Tenant\Avanzaempresa::where('slug','=',$page)->get();
    $menu = \DigitalsiteSaaS\Pagina\Tenant\Page::whereNull('page_id')->orderBy('posta', 'desc')->get();
    $blogfoot = \DigitalsiteSaaS\Pagina\Tenant\Bloguero::inRandomOrder()->take(6)->get();
-   $usuario = Auth::user()->id;
-   $identificador = Avanzaempresa::where('usuario_id', '=', $usuario)->get();
+
+   $identificador = \DigitalsiteSaaS\Avanza\Tenant\Avanzaempresa::where('slug', '=', $page)->get();
    foreach ($identificador as $identificador){
    $productos =  \DigitalsiteSaaS\Pagina\Tenant\Fichaje::where('identificador','=',$identificador->id)->get();
    }
@@ -948,7 +948,6 @@ $categories = \DigitalsiteSaaS\Pagina\Tenant\Pais::all();
     }
   return Response::json($subcategories);
 }
-
 
 
 
