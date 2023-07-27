f5f526c16dfc477a4bc2b5faaa861ee4249206a8
DesarrolloSite <43462186+DesarrolloSite@users.noreply.github.com>
2023-07-26 22:20:25 -0500

    Ajustes pagina
    Ajustes pagina

--- views/configuracion/redes.blade.php
+++ views/configuracion/redes.blade.php
@@ -253,6 +253,15 @@
                                             </div>
                                         </div>
 
+                                            {{Form::hidden('terminos', $plantilla->terminos, array('class' => 'form-control','placeholder'=>'Ingrese horario'))}}
+                                            
+                                        <div class="form-group form-actions">
+                                            <div class="col-md-9 col-md-offset-3">
+                                                <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-angle-right"></i> Editar</button>
+                                                <button type="reset" class="btn btn-sm btn-warning"><i class="fa fa-repeat"></i> Cancelar</button>
+                                            </div>
+                                        </div>
+
                                       {{ Form::close() }} 
                                       @endforeach
 
